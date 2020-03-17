0a5c9bfec4e03a0d48585cb7804dc1213960bc6d
larrysmith <larrypsmith415@gmail.com>
2020-03-17 13:03:10 -0700

    Fix require statements in users.js for validateLoginInput and validateRegisterInput

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,8 +4,8 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
-const validateLoginInput = require('../../validation/register');
-const validateRegisterInput = require('../../validation/login');
+const validateLoginInput = require('../../validation/login');
+const validateRegisterInput = require('../../validation/register');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
