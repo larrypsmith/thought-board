9686880d13ec92ca5d66698bf41bc14562379d1b
larrysmith <larrypsmith415@gmail.com>
2020-03-16 21:49:50 -0700

    Add users test route

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const users = require('./routes/api/users');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello World"));
+app.use("/api/users", users);
 
 const db = require('./config/keys').mongoURI;
 mongoose
