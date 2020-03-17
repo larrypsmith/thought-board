b77790ea45f0c02b501414f6497a44d52a1198be
larrysmith <larrypsmith415@gmail.com>
2020-03-17 12:19:27 -0700

    Add user login and register validations

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const validateLoginInput = require('../../validation/register');
+const validateRegisterInput = require('../../validation/login');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
