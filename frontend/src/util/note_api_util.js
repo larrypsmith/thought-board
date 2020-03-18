a4e5063cefd28fb069ad4ae39190f4b5aa8a0eab
Spencer Iascone <spencer.iascone@gmail.com>
2020-03-18 15:34:14 -0700

    Add notes_container, edit note_api_util

--- frontend/src/util/note_api_util.js
+++ frontend/src/util/note_api_util.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const getBoardNotes = (id) => {
-    return axios.get(`/api/board/${id}/notes`);
+export const getBoardNotes = (board) => {
+    return axios.get(`/api/notes/${board.id}`);
 };
 
 export const writeNote = data => {
