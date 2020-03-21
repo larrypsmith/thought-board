e7c7f026e421d9b1da9265ae0a2b598dbce725d9
Brennan Romance <brennan.romance@gmail.com>
2020-03-21 13:09:58 -0400

    board box now half way interactive: navigating

--- frontend/src/components/boards/board_box.js
+++ frontend/src/components/boards/board_box.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { withRouter } from 'react-router-dom'
 import NoteBox from '../notes/note_box'
 import './board.scss'
+import panAndZoomHoc from 'react-pan-and-zoom-hoc';
+
+const InteractiveDiv = panAndZoomHoc('div');
 
 class BoardBox extends React.Component {
+
     constructor(props) {
-        super(props);
+        super(props)
+        this.state = {
+            x: 0.5,
+            y: 0.5,
+            scale: 1
+        };
     }
 
     componentDidMount() {
@@ -13,23 +22,47 @@ class BoardBox extends React.Component {
         fetchBoardNotes(id)
     }
 
+    handlePanAndZoom(x, y, scale) {
+        this.setState({ x, y, scale });
+    }
+
+    handlePanMove(x, y) {
+        this.setState({ x, y });
+    }
+
     render() {
-        const { notes } = this.props;
-        if (!notes || !notes.length) return null;
+        const { x, y, scale } = this.state;
+
+        if (!this.props.notes) {
+            return null;
+        }
+
+        const allNotes = [];
+        this.props.notes.map((note, idx) => {
+            allNotes.push(<NoteBox
+                note={note}
+                key={idx}
+                updateNote={this.props.updateNote}
+            />)
+        })
         return (
-            <div className="board-main">
-                {
-                    notes.map((note, idx) => (
-                        <NoteBox
-                            note={note}
-                            key={idx}
-                            updateNote={this.props.updateNote}
-                        />    
-                    ))
-                }
-            </div>
-        );
+            <InteractiveDiv
+                x={x}
+                y={y}
+                className="board-main"
+                scale={scale}
+                scaleFactor={Math.sqrt(2)}
+                minScale={1}
+                maxScale={2 ** 18}
+                onPanAndZoom={(x, y, scale) => this.handlePanAndZoom(x, y, scale)}
+                onPanMove={(x, y) => this.handlePanMove(x, y)}
+            >
+                <div style={{ position: 'absolute', top: `${y * 100}%`, left: `${x * 100}%`, width: 10, height: 10, backgroundColor: 'black' }} />
+                {allNotes}
+            </InteractiveDiv>
+        )
     }
 }
+
 
 export default withRouter(BoardBox);
