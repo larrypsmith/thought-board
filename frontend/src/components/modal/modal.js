a55710c76185a0b69638b54e21e4a7ae0b9c7de5
siascone <spencer.iascone@gmail.com>
2020-04-01 17:27:43 -0700

    remove defined but unused variable

--- frontend/src/components/modal/modal.js
+++ frontend/src/components/modal/modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {closeModal, openModal} from '../../actions/modal_actions';
+import {closeModal} from '../../actions/modal_actions';
 import {connect} from 'react-redux';
 import NoteComposeContainer from '../notes/note_compose_container';
 import NoteEditFormContainer from '../notes/note_edit_form_container';
