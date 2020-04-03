fdb2ae4eae758eb6bf57f88a9f0cb17ee8cae345
siascone <spencer.iascone@gmail.com>
2020-04-03 10:54:20 -0700

    fix show

--- frontend/src/actions/modal_actions.js
+++ frontend/src/actions/modal_actions.js
@@ -1,12 +1,11 @@
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 
-export const openModal = (modal, boardId, id, title, caption, url) => {
+export const openModal = (modal, boardId, title, caption, url) => {
     return {
         type: OPEN_MODAL,
         modal,
         boardId,
-        id,
         title,
         caption,
         url
