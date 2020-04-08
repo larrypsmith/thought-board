90757eb2a276cc36140d07d1fefcc9f2a412bf26
siascone <spencer.iascone@gmail.com>
2020-04-08 14:09:08 -0700

    edit connect modal css

--- frontend/src/components/notes/note_connector.js
+++ frontend/src/components/notes/note_connector.js
@@ -20,10 +20,12 @@ class NoteConnector extends React.Component {
                 <div className='connections'>
                     <div className='connect-div'>
                         <p className='connect-heading'>Non-connected Notes</p>
-                        <div className='connect'>{this.buildButtons(this.props.noteList)}</div>
+                        <div className='connect-btns-div'>
+                            <div className='connect'>{this.buildButtons(this.props.noteList)}</div>
+                        </div>
                     </div>
                     <div className='disconnect-div'>
-                        <p className='disconnect-heading'>Connected Notes</p>
+                        <p className='disconnect-heading'>Connected Notes: Click to Disconnect</p>
                         This is where the list of connected notes will go. A function to make this list will need to be made. A disconnect button should likely be mapped along side the connected notes titles when the list of connected notes is maped.
                     </div>
                 </div>
